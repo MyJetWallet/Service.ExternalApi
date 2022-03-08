102aa859948198b980b7da45e13f2a10f51e7d05
Maxim Gorin <mxmgorin@gmail.com>
2022-03-08 14:48:36 +0200

    Use structured logging

--- src/Service.ExternalApi/Services/ExternalMarketApi.cs
+++ src/Service.ExternalApi/Services/ExternalMarketApi.cs
@@ -27,7 +27,7 @@ namespace Service.ExternalApi.Services
 
         public async Task<GetNameResult> GetNameAsync(GetNameRequest request)
         {
-            _logger.LogInformation("GetNameAsync receive request {requestJson}", JsonConvert.SerializeObject(request));
+            _logger.LogInformation("GetNameAsync receive request {@request}", request);
             
             ProxyHelper.ValidateExchangeName(_logger, request.ExchangeName);
 
@@ -37,7 +37,6 @@ namespace Service.ExternalApi.Services
                 
                 if (exchange == null)
                 {
-                    _logger.LogError($"Cannot find exchange: {request.ExchangeName}");
                     throw new Exception($"Cannot find exchange: {request.ExchangeName}");
                 }
 
@@ -45,15 +44,13 @@ namespace Service.ExternalApi.Services
 
                 var exchangeResponse = await exchange.GetNameAsync(request);
 
-                _logger.LogInformation("Exchange Response: {exchangeResponseJson}",
-                    JsonConvert.SerializeObject(exchangeResponse));
+                _logger.LogInformation("GetNameAsync Exchange Response: {@exchangeResponse}", exchangeResponse);
 
                 return exchangeResponse;
             }
             catch (Exception ex)
             {
-                _logger.LogError("GetNameAsync receive exception: {exJson}",
-                    JsonConvert.SerializeObject(ex));
+                _logger.LogError(ex, "Failed GetNameAsync: {@request}", request);
                 return null;
             }
         }
@@ -68,25 +65,21 @@ namespace Service.ExternalApi.Services
 
                 if (exchange == null)
                 {
-                    _logger.LogError($"Cannot find exchange: {request.ExchangeName}");
-                    return new GetBalancesResponse()
-                    {
-                        Balances = new List<ExchangeBalance>()
-                    };
+                    throw new Exception($"Cannot find exchange: {request.ExchangeName}");
                 }
 
-                _logger.LogInformation($"Exchange:{request.ExchangeName}");
+                _logger.LogInformation($"GetBalancesAsync Exchange:{request.ExchangeName}");
 
                 var exchangeResponse = await exchange.GetBalancesAsync(request);
                 exchangeResponse.Balances ??= new List<ExchangeBalance>();
 
-                _logger.LogInformation("Exchange Response Balances count: {count}", exchangeResponse.Balances?.Count);
+                _logger.LogInformation("GetBalancesAsync Exchange Response: {@response}", exchangeResponse);
 
                 return exchangeResponse;
             }
             catch (Exception ex)
             {
-                _logger.LogError("GetBalancesAsync receive exception: {exJson}", JsonConvert.SerializeObject(ex));
+                _logger.LogError(ex, "Failed GetBalancesAsync receive exception: {@request}", request);
                 
                 return new GetBalancesResponse()
                 {
@@ -97,7 +90,7 @@ namespace Service.ExternalApi.Services
 
         public async Task<GetMarketInfoResponse> GetMarketInfoAsync(MarketRequest request)
         {
-            _logger.LogInformation("GetMarketInfoAsync receive request {requestJson}", JsonConvert.SerializeObject(request));
+            _logger.LogInformation("GetMarketInfoAsync receive request {@request}", request);
             
             try
             {
@@ -105,29 +98,27 @@ namespace Service.ExternalApi.Services
                 
                 if (exchange == null)
                 {
-                    _logger.LogError($"Cannot find exchange: {request.ExchangeName}");
                     throw new Exception($"Cannot find exchange: {request.ExchangeName}");
                 }
 
-                _logger.LogInformation($"Exchange: {request.ExchangeName}");
+                _logger.LogInformation($"GetMarketInfoAsync Exchange: {request.ExchangeName}");
 
                 var exchangeResponse = await exchange.GetMarketInfoAsync(request);
 
-                _logger.LogInformation("Exchange Response: {exchangeResponseJson}", JsonConvert.SerializeObject(exchangeResponse));
+                _logger.LogInformation("GetMarketInfoAsync Exchange Response: {@exchangeResponse}", exchangeResponse);
 
                 return exchangeResponse;
             }
             catch (Exception ex)
             {
-                _logger.LogError("GetMarketInfoAsync receive exception: {exJson}",
-                    JsonConvert.SerializeObject(ex));
+                _logger.LogError(ex, "Failed GetMarketInfoAsync {@request}", request);
                 return null;
             }
         }
 
         public async Task<GetMarketInfoListResponse> GetMarketInfoListAsync(GetMarketInfoListRequest request)
         {
-            _logger.LogInformation("GetMarketInfoListAsync receive request {requestJson}", JsonConvert.SerializeObject(request));
+            _logger.LogInformation("GetMarketInfoListAsync receive request {@request}", request);
             
             ProxyHelper.ValidateExchangeName(_logger, request.ExchangeName);
 
@@ -137,30 +128,27 @@ namespace Service.ExternalApi.Services
                 
                 if (exchange == null)
                 {
-                    _logger.LogError($"Cannot find exchange: {request.ExchangeName}");
                     throw new Exception($"Cannot find exchange: {request.ExchangeName}");
                 }
 
-                _logger.LogInformation($"Exchange: {request.ExchangeName}");
+                _logger.LogInformation($"GetMarketInfoListAsync Exchange: {request.ExchangeName}");
 
                 var exchangeResponse = await exchange.GetMarketInfoListAsync(request);
 
-                _logger.LogInformation("Exchange Response: {exchangeResponseJson}",
-                    JsonConvert.SerializeObject(exchangeResponse));
+                _logger.LogInformation("GetMarketInfoListAsync ExchangeResponse: {@exchangeResponse}", exchangeResponse);
 
                 return exchangeResponse;
             }
             catch (Exception ex)
             {
-                _logger.LogError("GetMarketInfoListAsync receive exception: {exJson}",
-                    JsonConvert.SerializeObject(ex));
+                _logger.LogError(ex, "Failed GetMarketInfoListAsync {@request}", request);
                 return null;
             }
         }
 
         public async Task<ExchangeTrade> MarketTrade(MarketTradeRequest request)
         {
-            _logger.LogInformation("MarketTrade receive request {requestJson}", JsonConvert.SerializeObject(request));
+            _logger.LogInformation("MarketTrade receive request {@request}", request);
             
             ProxyHelper.ValidateExchangeName(_logger, request.ExchangeName);
 
@@ -170,25 +158,22 @@ namespace Service.ExternalApi.Services
                 
                 if (exchange == null)
                 {
-                    _logger.LogError($"Cannot find exchange: {request.ExchangeName}");
                     throw new Exception($"Cannot find exchange: {request.ExchangeName}");
                 }
 
-                _logger.LogInformation($"Exchange: {request.ExchangeName}");
+                _logger.LogInformation($"MarketTrade Exchange: {request.ExchangeName}");
                 
                 _externalApiMetrics.SetMetrics(request);
                 
                 var exchangeResponse = await exchange.MarketTrade(request);
 
-                _logger.LogInformation("Exchange Response: {exchangeResponseJson}",
-                    JsonConvert.SerializeObject(exchangeResponse));
+                _logger.LogInformation("MarketTrade Exchange Response: {@exchangeResponse}", exchangeResponse);
 
                 return exchangeResponse;
             }
             catch (Exception ex)
             {
-                _logger.LogError("MarketTrade receive exception: {exJson}",
-                    JsonConvert.SerializeObject(ex));
+                _logger.LogError(ex, "Failed MarketTrade {@request}", request);
                 return null;
             }
         }
